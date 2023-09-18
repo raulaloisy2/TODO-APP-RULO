3984c005c053c9b4683d844e33e2d9d742830683
Raul Aloisy Castro Cota <raul_aloisy@live.com.mx>
2023-09-18 00:36:03 -0700

    Upload Frames

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
+import SignUp from './auth/SignUp'; 
+import Login from './auth/Login'; 
+import ForgotPass from './auth/ForgotPass';
+import ChangePass from './auth/ChangePass';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <ChangePass /> {}
     </View>
   );
 }
